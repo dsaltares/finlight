0cbc6cffcfb7deac8c16d70133a4f7086ed557af
David Saltares <david.saltares@gmail.com>
2023-07-02 20:42:53 +0300

    chore: replace tailwind with material ui

--- src/pages/_app.next.tsx
+++ src/pages/_app.next.tsx
@@ -1,10 +1,14 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import type { AppProps } from 'next/app';
 import { useState } from 'react';
 import { SessionProvider } from 'next-auth/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { Session } from 'next-auth';
-import '@styles/globals.css';
+import CssBaseline from '@mui/material/CssBaseline';
 
 type PageProps = {
   session: Session | null;
@@ -34,6 +38,7 @@ const App = ({
     <SessionProvider session={session}>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
+        <CssBaseline />
         <Component {...pageProps} />
       </QueryClientProvider>
     </SessionProvider>
