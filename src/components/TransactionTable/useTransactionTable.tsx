bfe6b3affd4383349fa91a559559cdb5058518ee
David Saltares <david.saltares@gmail.com>
2023-07-13 10:51:46 +0300

    refactor: split transaction table logic from visuals

--- src/components/TransactionTable/useTransactionTable.tsx
+++ src/components/TransactionTable/useTransactionTable.tsx
@@ -6,51 +6,31 @@ import {
   type FilterFn,
   type Row,
   createColumnHelper,
-  flexRender,
   getCoreRowModel,
   getFilteredRowModel,
   getSortedRowModel,
   useReactTable,
 } from '@tanstack/react-table';
 import stringToColor from 'string-to-color';
+import Checkbox from '@mui/material/Checkbox';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Typography from '@mui/material/Typography';
-import Link from 'next/link';
-import Paper from '@mui/material/Paper';
-import TableContainer from '@mui/material/TableContainer';
-import Table from '@mui/material/Table';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import TableCell from '@mui/material/TableCell';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import TableBody from '@mui/material/TableBody';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import PaymentIcon from '@mui/icons-material/Payment';
 import PaidIcon from '@mui/icons-material/Paid';
 import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
-import IconButton from '@mui/material/IconButton';
-import Stack from '@mui/material/Stack';
 import Chip from '@mui/material/Chip';
-import Checkbox from '@mui/material/Checkbox';
+import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
 import { useTheme } from '@mui/material/styles';
-import useFiltersFromurl from '@lib/useFiltersFromUrl';
-import useSortFromUrl from '@lib/useSortFromUrl';
 import type { Account } from '@server/account/types';
 import type { Category } from '@server/category/types';
-import type {
-  Transaction,
-  UpdateTransactionsInput,
-} from '@server/transaction/types';
+import type { Transaction } from '@server/transaction/types';
 import { formatAmount, formatDate } from '@lib/format';
-import useDialogForId from '@lib/useDialogForId';
-import useDeleteTransactions from '@lib/transactions/useDeleteTransactions';
-import useUpdateTransaction from '@lib/transactions/useUpdateTransaction';
-import useDialogFromUrl from '@lib/useDialogFromUrl';
-import useUpdateTransactions from '@lib/transactions/useUpdateTransactions';
-import ConfirmationDialog from './ConfirmationDialog';
-import CreateUpdateTransactionDialog from './CreateUpdateTransactionDialog';
-import UpdateTransactionsDialog from './UpdateTransactionsDialog';
+import useSortFromUrl from '@lib/useSortFromUrl';
+import useFiltersFromurl from '@lib/useFiltersFromUrl';
 
 declare module '@tanstack/table-core' {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -63,7 +43,7 @@ declare module '@tanstack/table-core' {
   }
 }
 
-const DefaultSort = { id: 'date', desc: true };
+export const DefaultSort = { id: 'date', desc: true };
 
 const toTransactionTableRow = (
   transaction: Transaction,
@@ -88,47 +68,23 @@ type Props = {
   categories: Category[];
   rowSelection: RowSelectionState;
   onRowSelectionChange: OnChangeFn<RowSelectionState>;
-  multiDeleteOpen: boolean;
-  onMultiDeleteClose: () => void;
-  multiUpdateOpen: boolean;
-  onMultiUpdateClose: () => void;
+  onUpdateDialogOpen: (id: string) => void;
+  onDeleteDialogOpen: (id: string) => void;
 };
 
-const TransactionTable = ({
+const useTransactionTable = ({
   transactions,
-  categories,
   accounts,
+  categories,
   rowSelection,
   onRowSelectionChange,
-  multiDeleteOpen,
-  onMultiDeleteClose,
-  multiUpdateOpen,
-  onMultiUpdateClose,
+  onUpdateDialogOpen,
+  onDeleteDialogOpen,
 }: Props) => {
   const theme = useTheme();
   const { query } = useRouter();
-  const { sorting, toggleSort } = useSortFromUrl(DefaultSort);
+  const { sorting } = useSortFromUrl(DefaultSort);
   const { filters } = useFiltersFromurl();
-  const {
-    openFor: deleteOpenFor,
-    open: isDeleteDialogOpen,
-    onOpen: onDeleteDialogOpen,
-    onClose: onDeleteDialogClose,
-  } = useDialogForId();
-  const {
-    openFor: transactionId,
-    open: isUpdateDialogOpen,
-    onOpen: onUpdateDialogOpen,
-    onClose: onUpdateDialogClose,
-  } = useDialogFromUrl('transactionId');
-
-  const { mutateAsync: updateTransaction, isLoading: isUpdating } =
-    useUpdateTransaction();
-  const transaction = useMemo(
-    () => transactions?.find((transaction) => transaction.id === transactionId),
-    [transactions, transactionId]
-  );
-
   const tableTransactions = useMemo(() => {
     const accountsById = (accounts || []).reduce(
       (acc, account) => ({
@@ -289,7 +245,7 @@ const TransactionTable = ({
     [query, onDeleteDialogOpen, onUpdateDialogOpen, theme.palette]
   );
 
-  const table = useReactTable({
+  return useReactTable({
     data: tableTransactions,
     columns,
     state: {
@@ -320,130 +276,6 @@ const TransactionTable = ({
     getFilteredRowModel: getFilteredRowModel(),
     onRowSelectionChange: onRowSelectionChange,
   });
-
-  const { mutateAsync: deleteTransactions, isLoading: isDeleting } =
-    useDeleteTransactions();
-  const handleSingleDelete = () =>
-    deleteOpenFor ? deleteTransactions({ ids: [deleteOpenFor] }) : undefined;
-  const handleMultiDelete = () =>
-    deleteTransactions({
-      ids: table.getSelectedRowModel().flatRows.map((row) => row.original.id),
-    });
-  const { mutateAsync: updateTransactions, isLoading: isMultiUpdating } =
-    useUpdateTransactions();
-
-  const handleMultiUpdate = (data: Omit<UpdateTransactionsInput, 'ids'>) =>
-    updateTransactions({
-      ids: table.getSelectedRowModel().flatRows.map((row) => row.original.id),
-      ...data,
-    });
-
-  return (
-    <Paper elevation={0} variant="outlined">
-      <TableContainer>
-        <Table sx={{ minWidth: 650 }} size="small">
-          <TableHead>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <TableCell
-                    key={header.id}
-                    sortDirection={header.column.getIsSorted()}
-                    align={
-                      header.getContext().column.columnDef.meta?.numeric
-                        ? 'right'
-                        : 'left'
-                    }
-                  >
-                    {header.column.columnDef.enableSorting !== false ? (
-                      <TableSortLabel
-                        active={!!header.column.getIsSorted()}
-                        direction={header.column.getIsSorted() || undefined}
-                        onClick={() => toggleSort(header.column.id)}
-                      >
-                        {flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                      </TableSortLabel>
-                    ) : (
-                      flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )
-                    )}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))}
-          </TableHead>
-          <TableBody>
-            {table.getRowModel().rows.map((row) => (
-              <TableRow key={row.id} hover>
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell
-                    key={cell.id}
-                    align={
-                      cell.column.columnDef.meta?.numeric ? 'right' : 'left'
-                    }
-                  >
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <ConfirmationDialog
-        id="delete-transaction"
-        title="Delete transaction"
-        open={isDeleteDialogOpen}
-        loading={isDeleting}
-        onClose={onDeleteDialogClose}
-        onConfirm={handleSingleDelete}
-      >
-        <Typography variant="body1">
-          Are you sure you want to delete this transaction? The action cannot be
-          undone.
-        </Typography>
-      </ConfirmationDialog>
-      <ConfirmationDialog
-        id="delete-transactions"
-        title="Delete transactions"
-        open={multiDeleteOpen}
-        loading={isDeleting}
-        onClose={onMultiDeleteClose}
-        onConfirm={handleMultiDelete}
-      >
-        <Typography variant="body1">
-          {`Are you sure you want to delete ${
-            Object.keys(rowSelection).length
-          } transactions? The action cannot be undone.`}
-        </Typography>
-      </ConfirmationDialog>
-      {!!transaction && (
-        <CreateUpdateTransactionDialog
-          transaction={transaction}
-          open={isUpdateDialogOpen}
-          loading={isUpdating}
-          accounts={accounts || []}
-          categories={categories || []}
-          onClose={onUpdateDialogClose}
-          onUpdate={updateTransaction}
-        />
-      )}
-      {!!multiUpdateOpen && (
-        <UpdateTransactionsDialog
-          open={multiUpdateOpen}
-          loading={isMultiUpdating}
-          categories={categories || []}
-          onClose={onMultiUpdateClose}
-          onUpdate={handleMultiUpdate}
-        />
-      )}
-    </Paper>
-  );
 };
 
-export default TransactionTable;
+export default useTransactionTable;
