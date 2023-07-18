30af4a25fd5c3fdb0de45125113c6b872ada58ba
David Saltares <david.saltares@gmail.com>
2023-07-18 15:56:25 +0300

    fix: logo text part of typography

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,18 @@
-import Link from "next/link";
-import Image from "next/image";
-import Routes from "@lib/routes";
+import Link from 'next/link';
+import Image from 'next/image';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import Routes from '@lib/routes';
 
-const Logo = () => <Link href={Routes.home}><Image alt="logo" width={100} height={90} src="/logo-regular.svg"/></Link>;
+const Logo = () => (
+  <Link href={Routes.home} style={{ textDecoration: 'none' }}>
+    <Stack alignItems="center" gap={0.1}>
+      <Image alt="logo" width={100} height={90} src="/logo-no-text.svg" />
+      <Typography color="primary" fontWeight="bold" sx={{ fontSize: 24 }}>
+        FINLIGHT
+      </Typography>
+    </Stack>
+  </Link>
+);
 
 export default Logo;
