97364cf4c9ac3cf57fc4a6c09c978aaec1e49f95
David Saltares <david.saltares@gmail.com>
2023-07-26 11:21:24 +0300

    fix: cron job checks if it needs to fetch rates in the past

--- src/pages/api/cron/updateExchangeRates.route.ts
+++ src/pages/api/cron/updateExchangeRates.route.ts
@@ -2,6 +2,9 @@
 import format from 'date-fns/format';
 import startOfYesterday from 'date-fns/startOfYesterday';
 import type { NextApiHandler } from 'next';
+import isToday from 'date-fns/isToday';
+import addDays from 'date-fns/addDays';
+import chunk from 'lodash.chunk';
 import prisma from '@server/prisma';
 import { PolygonGroupedDailyFX } from '@lib/polygon';
 
@@ -16,11 +19,38 @@ const handler: NextApiHandler = async (req, res) => {
     return;
   }
 
-  const date = startOfYesterday();
+  const latestRate = await prisma.exchangeRate.findFirst({
+    orderBy: { date: 'desc' },
+  });
+
+  const dates: Date[] = [];
+  let dateTmp = latestRate?.date || startOfYesterday();
+  while (!isToday(dateTmp)) {
+    dates.push(dateTmp);
+    dateTmp = addDays(dateTmp, 1);
+  }
+
+  const dateChunks = chunk(dates, 5);
+  for (const dateChunk of dateChunks) {
+    await Promise.all(dateChunk.map(importRates));
+    console.log('Waiting 60 seconds...');
+    await new Promise((resolve) => setTimeout(resolve, 60 * 1000));
+  }
+
+  let date = latestRate?.date || startOfYesterday();
+  while (!isToday(date)) {
+    date = addDays(date, 1);
+  }
+  res.status(200).end();
+};
+
+export default handler;
+
+const importRates = async (date: Date) => {
   const formattedDate = format(date, 'yyyy-MM-dd');
   console.log(`Fetching exchange rates for ${formattedDate}`);
   const response = await fetch(
-    `https://api.polygon.io/v2/aggs/grouped/locale/global/market/fx/${formattedDate}?adjusted=true&apiKey=${process.env.POLYGON_API_KEY}`
+    `https://api.polygon.io/v2/aggs/grouped/locale/global/market/fx/${formattedDate}?adjusted=true&apiKey=${process.env.POLYGON_API_KEY}`,
   );
   const rates = (
     PolygonGroupedDailyFX.parse(await response.json()).results || []
@@ -37,7 +67,4 @@ const handler: NextApiHandler = async (req, res) => {
     })),
     skipDuplicates: true,
   });
-  res.status(200).end();
 };
-
-export default handler;
